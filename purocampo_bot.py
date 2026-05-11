87fcf8ec1cc6d0ce40b2c617aba563b27965122d
jeissonmaldonado1115-bit <jeissonmaldonado1115@gmail.com>
2026-05-11 12:26:28 -0500

    Update purocampo_bot.py

--- purocampo_bot.py
+++ purocampo_bot.py
@@ -375,7 +375,7 @@ def main():
             CONFIRMACION:       [MessageHandler(filters.TEXT & ~filters.COMMAND, confirmar)],
         },
         fallbacks=[CommandHandler("cancelar", cancelar)],
-        allow_reentry=false,
+        allow_reentry=False,
     )
 
     app.add_handler(conv)
